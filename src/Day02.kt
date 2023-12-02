61f5c618b6f5472d622b6e5c92c4af0be42ef45e
Schmowser <schmowser@posteo.de>
2023-12-02 07:55:52 +0100

    Solution Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,13 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            val gameId = line.substringBefore(":").removePrefix("Game ").toInt()
+            val isGamePossible = line.substringAfter(": ").split("; ")
+                .all { it.containsLessBallsThenMax() }
+            gameId.takeIf { isGamePossible } ?: 0
+        }.reduce { acc, i -> acc + i }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            val colorMaxMap = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+            line.substringAfter(": ").split("; ")
+                .forEach { set ->
+                    set.split(", ")
+                        .map { it.substringBefore(" ").toInt() to it.substringAfter(" ") }
+                        .forEach { if (it.first > (colorMaxMap[it.second] ?: Int.MAX_VALUE)) colorMaxMap[it.second] = it.first }
+                }
+            colorMaxMap.values.reduce { acc, i -> acc * i }
+        }.reduce { acc, i -> acc + i }
     }
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
+}
+
+private fun String.containsLessBallsThenMax(): Boolean {
+    val colorMaxMap = mapOf("red" to 12, "green" to 13, "blue" to 14)
+    return this.split(", ").all {
+        val amountOfBalls = it.substringBefore(" ").toInt()
+        val ballColor = it.substringAfter(" ")
+        amountOfBalls <= (colorMaxMap[ballColor] ?: -Int.MAX_VALUE)
+    }
 }
