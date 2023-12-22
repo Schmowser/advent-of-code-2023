bef6edca1d877ee5b5578f6674d374c6571ca87b
Schmowser <schmowser@posteo.de>
2023-12-22 23:11:09 +0100

    Solution Day 22 Part 1

--- src/Day22.kt
+++ src/Day22.kt
@@ -4,32 +4,59 @@ fun main() {
             val firstSecond = line.split("~")
             val first = firstSecond[0].split(",")
             val second = firstSecond[1].split(",")
-            Triple(first[0].toInt()..second[0].toInt(), first[1].toInt()..second[1].toInt(), first.last().toInt())
-        }.sortedBy { it.third }
+            Triple(first[0].toInt()..second[0].toInt(), first[1].toInt()..second[1].toInt(), first[2].toInt()..second[2].toInt())
+        }.sortedBy { it.third.min() }
         println(sortedInput)
 
-        val fallenDownBricks = sortedInput.mapIndexed { index, brick ->
-            if (index > 0) {
-                // TODO: Not only check sortedInput[index - 1] but all the bricks before
-                val intersectX = sortedInput[index - 1].first.intersect(sortedInput[index].first)
-                val intersectY = sortedInput[index - 1].second.intersect(sortedInput[index].second)
-                println(intersectX)
-                println(intersectY)
-                println("----")
+        val fallenDownBricks = mutableListOf<Triple<IntRange, IntRange, IntRange>>()
+        fallenDownBricks.add(sortedInput[0])
+        sortedInput.takeLast(sortedInput.size - 1).forEach { brick ->
+            val highestSupportList = fallenDownBricks.filter {
+                it.first.intersect(brick.first).isNotEmpty() && it.second.intersect(brick.second).isNotEmpty()
+            }
 
-                if (intersectX.isNotEmpty() && intersectY.isNotEmpty()) {
-                    Triple(brick.first, brick.second, sortedInput[index - 1].third + 1)
-                } else {
-                    Triple(brick.first, brick.second, sortedInput[index - 1].third)
-                }
-            } else brick
+            if (highestSupportList.isNotEmpty()) {
+                val highestSupport = highestSupportList.maxBy { it.third.last }
+                fallenDownBricks.add(
+                    Triple(
+                        brick.first,
+                        brick.second,
+                        (highestSupport.third.last + 1)..(highestSupport.third.last + 1 + (brick.third.last - brick.third.first))
+                    )
+                )
+            } else {
+                fallenDownBricks.add(
+                    Triple(brick.first, brick.second, 1..(1 + (brick.third.last - brick.third.first)))
+                )
+            }
+
+            // fallenDownBricks.sortBy { it.third.last }
         }
         fallenDownBricks.println()
 
-        // TODO: Add the intersections to the currentBrick
+        val brickIndexToSupportedByIndex =
+                fallenDownBricks.associateWith { mutableSetOf<Triple<IntRange, IntRange, IntRange>>() }
+        for (bottomLevel in 1..fallenDownBricks.maxOf { it.third.first }) {
+            fallenDownBricks.filter { it.third.last == bottomLevel - 1 }.forEach { lowerBrick ->
+                fallenDownBricks.filter { it.third.first == bottomLevel }.forEach { upperBrick ->
+                    if (lowerBrick.first.intersect(upperBrick.first).isNotEmpty() && lowerBrick.second.intersect(upperBrick.second).isNotEmpty()) {
+                        brickIndexToSupportedByIndex[upperBrick]?.add(lowerBrick)
+                    }
+                }
+            }
+        }
+        brickIndexToSupportedByIndex.println()
 
-        // TODO: Filter for the bricks that are not the only support for any other brick
-        return input.size
+        val unremovableBricks = fallenDownBricks.filter { brick ->
+            brickIndexToSupportedByIndex.values.filter { supports -> supports.contains(brick) }
+                .any { it.size == 1 }
+        }
+        //unremovableBricks.println()
+
+        input.size.println()
+        brickIndexToSupportedByIndex.filter { it.value.size == 1 }.values.distinct().size.println()
+
+        return fallenDownBricks.size - unremovableBricks.size
     }
 
     fun part2(input: List<String>): Int {
@@ -42,10 +69,10 @@ fun main() {
     val testResultPart1 = part1(tinput)
     testResultPart1.println()
     println("Test 1 Part 1 succeeded: ${testResultPart1 == 5}")
-//    val resultPart1 = part1(input)
-//    resultPart1.println()
-//    println("Part 1 succeeded: ${resultPart1 == 114400}")
-//
+    val resultPart1 = part1(input)
+    resultPart1.println()
+    println("Part 1 succeeded: ${resultPart1 == 393}")
+
 //    val testResultPart2 = part2(tinput)
 //    testResultPart2.println()
 //    println("Test 1 Part 2 succeeded: ${testResultPart2 == 2}")
