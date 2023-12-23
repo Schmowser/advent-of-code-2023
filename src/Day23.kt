44d028a448d82ac31976cb0d33d13968e55abe82
Schmowser <schmowser@posteo.de>
2023-12-23 08:02:15 +0100

    Solution Day 23 Part 2 A

--- src/Day23.kt
+++ src/Day23.kt
@@ -28,7 +28,44 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val winput = input.map { line ->
+            line.replace('^', '.').replace('>', '.').replace('v', '.').replace('<', '.')
+        }
+        println(winput.joinToString("\n"))
+
+        var result = 0
+        val pathList = mutableListOf<Triple<Pair<Int, Int>, Direction, Int>>()
+        // currentPos, comingFrom, pathLength
+        pathList.add(Triple(0 to 1, Direction.NORTH, 0))
+
+        while (pathList.any { path -> path.first != input.size - 1 to input.last().length - 2 }) {
+            for (i in 0 until pathList.size) {
+                val currentPos = pathList[i].first
+                val nextValidDirections = mutableSetOf<Direction>()
+                val comingFrom = pathList[i].second
+                for (dir in Direction.entries.minus(comingFrom)) {
+                    val nextTile = input[currentPos.first + dir.vertical].elementAt(currentPos.second + dir.horizontal)
+                    when {
+                        nextTile == '.' -> nextValidDirections.add(dir)
+                        setOf('^', '>', '<', 'v').contains(nextTile) -> nextValidDirections.add(dir)
+                    }
+                }
+
+                val nextDirection = nextValidDirections.first() // TODO: Split if there is a crossing
+                // TODO: Do not go where we already were
+
+                pathList[i] = Triple(
+                    currentPos.first + nextDirection.vertical to currentPos.second + nextDirection.horizontal,
+                    nextDirection.getCounterDirection(),
+                    result++
+                )
+
+                println("Stepping $nextDirection to $currentPos coming from $comingFrom")
+            }
+        }
+
+        println(pathList)
+        return pathList.maxOf { it.third }
     }
 
     val tinput = readInput("Day23test")
